bf15d81619ad1438241b2ae1b3316820c68a63c1
Kavishka Indeepa <101946925+KavishkaIndeepa@users.noreply.github.com>
2023-05-25 23:32:18 +0530

    added loging form

---
+++ src/controller/LoginFormController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.input.KeyEvent;
+
+public class LoginFormController {
+    public JFXTextField txtName;
+
+    public void KeyOnAction(KeyEvent keyEvent) {
+    }
+
+    public void LoggingOnAction(ActionEvent actionEvent) {
+    }
+}
